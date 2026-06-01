b7e833c55aea77cdfa301c332349a21f3e5cdad5
Chris <cgkocur@gmail.com>
2026-05-31 22:54:51 -0400

    updated the jsdoc

--- classes/BasicQueue.mjs
+++ classes/BasicQueue.mjs
@@ -1,13 +1,21 @@
+/**
+ * Basic Queue implementation
+ */
 export class BasicQueue {
     constructor(){
         this.items = []
     }
+    /**
+     * Adds element to the queue.
+     * @param {*} element thing you want added to the queue, can be anything.
+     * @returns {void}
+     */
     enqueue(element){    
         this.items.push(element)
     }
     /**
-     * 
-     * @returns {false|any} - Returns false if empty 
+     * Removes an item from the queue and returns it.
+     * @returns {false|any} Returns false if empty 
      */
     dequeue(){
         if(this.isEmpty()){
@@ -16,7 +24,7 @@ export class BasicQueue {
         return this.items.shift();
     }
     /**
-     * 
+     * Returns a copy of the first item in the queue. To remove use dequeue.
      * @returns {false|any} - Returns false if empty 
      */
     peek(){
@@ -25,9 +33,17 @@ export class BasicQueue {
         }
         return this.items[0]
     }
+    /**
+     * Used to check if queue is empty.
+     * @returns {boolean}
+     */
     isEmpty(){
         return this.items.length === 0
     }
+    /**
+     * Returns an array of whatever you put in the queue.
+     * @returns {*[]}
+     */
     printQueue(){
         let list = []
         this.items.forEach(item => {
