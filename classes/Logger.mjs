1ac232504718c99d7bcc592cf3c2342674f2c2a3
Chris <cgkocur@gmail.com>
2026-06-01 23:24:12 -0400

    corrected things and now working implementation for basic needs

--- classes/Logger.mjs
+++ classes/Logger.mjs
@@ -1,67 +1,132 @@
-import { appendFile } from 'fs/promises'
+import fs from 'fs/promises'
 
 /**
  * A basic logger that writes to console and file with the same actions. Mimics log, info, warn, and error of console.
- * @class
  */
-class Logger {
+export class Logger {
 
-    constructor(){
-        let now = new Date(Date.now());
-        this.fileName = this.getDateFormatted().date
-        appendFile(`../logs/${this.fileName}`, `${this.getDateFormatted().time} => New Logging Session Started`)
+    static LEVELS = {
+        DEBUG: 0,
+        INFO: 1,
+        WARN: 2,
+        ERROR: 3
+    }
+
+    static LoggingLevel = Logger.LEVELS.DEBUG
+    /**
+     * 
+     * @param {string} filePath path to the logger file directory you want to log to. If none is provided it will create a file in the same folder as the class as a fallback.
+     * @param {string} context this will add a label in the logger to provide origin context to the line.
+     */
+    constructor(filePath = "./", context = "unassigned"){
+        let now = new Date(Date.now())
+        /** @type {string} */
+        this.filePath = `${filePath}/${this.getCurrentDateTimeFormatted().date}.txt`
+        /** @type {string} */
+        this.context = context
+        this.updateLog()
+    }
+
+    /**
+     * This method is run in the constructor to add a line to show when a new session has started.
+     */
+    async updateLog(){
+        this.log("New Logging Session Started", {tags: ["NewSession"]})
     }
 
     /**
      * Method to get date and time based on current time. Use returned object properties to get uniform formatting.     * 
      */
-    getDateFormatted(){
-        let now = new Date(Date.now()),
+    getCurrentDateTimeFormatted(){
+        let now = new Date(Date.now())
+        console.log(now.toISOString())
         
         return {
             date: `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`,
-            time: `${now.getHours}:${now.getMinutes}:${now.getSeconds}`
+            time: `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
         }
     }
+
     /**
-     * 
-     * @param {string} context name of the method or context label of thing you want to log 
-     * @param {string} msg string of the message you'd like to add to the log
+     * @typedef LoggingOptions
+     * @prop {string[]} tags
      */
-    log(context = "unassigned", msg = ""){
-        let line = `${this.getDateFormatted().time} => ${context}: ${msg}`
+
+    /**
+     * @param {string} msg
+     * @param {LoggingOptions} options optional flags for logging
+     */
+    async log(msg, options){
+        let line = `** LOG ** ${await this._generateLine(msg, options)}`
         console.log(line)
-        appendFile(this.fileName, `** LOG ** ${line}`)
+        this._write(line)
     }
     /**
-     * 
-     * @param {string} context name of the method or context label of thing you want to log 
-     * @param {string} msg string of the message you'd like to add to the log
+     * @param {string} msg
+     * @param {Object} options optional flags for logging
+     * @param {LoggingOptions} options.tags tags to help aggregate or search
      */
-    info(context = "unassigned", msg = ""){
-        let line = `I${this.getDateFormatted().time} => ${context}: ${msg}`
+    async info(msg, options){
+        let line = `** INFO ** ${await this._generateLine(msg, options)}`
         console.info(line)
-        appendFile(this.fileName, `** INFO ** ${line}`)
+        this._write(line)
     }
     /**
-     * 
-     * @param {string} context name of the method or context label of thing you want to log 
-     * @param {string} msg string of the message you'd like to add to the log
+     * @param {string} msg
+     * @param {Object} options optional flags for logging
+     * @param {LoggingOptions} options.tags tags to help aggregate or search
      */
-    warn(context = "unassigned", msg = ""){
-        let line = `${this.getDateFormatted().time} => ${context}: ${msg}`
+    async warn(msg, options){
+        let line = `** WARN ** ${await this._generateLine(msg, options)}`
         console.warn(line)
-        appendFile(this.fileName, `** WARN ** ${line}`)
+        this._write(line)
     }
     /**
-     * 
-     * @param {string} context name of the method or context label of thing you want to log 
-     * @param {string} msg string of the message you'd like to add to the log
+     * @param {string} msg
+     * @param {Object} options optional flags for logging
+     * @param {LoggingOptions} options.tags tags to help aggregate or search
      */
-    error(context = "unassigned", msg = ""){
-        let line = `${this.getDateFormatted().time} => ${context}: ${msg}`
+    async error(msg, options){
+        let line = `** ERROR ** ${await this._generateLine(msg, options)}`
         console.error(line)
-        appendFile(this.fileName, `** ERROR ** ${line}`)
+        this._write(line)
+    }
+
+    /**
+     * A function to
+     * @param {string} msg 
+     * @param {LoggingOptions} options 
+     * @returns {string}
+     */
+    async _generateLine(msg = "", options = {tags:[]}){
+        let line = `${this.getCurrentDateTimeFormatted().time} => ${this.context}: ${msg}`
+        if (options.tags && options.tags.length > 0){
+            for (const tag of options.tags) {
+                line = line.concat(" #", tag)
+            }
+        }
+        return line.concat("\n")
+    }
+
+    async _write(msg){
+        try {
+            await fs.appendFile(this.filePath, msg)
+        } catch (err) {
+            console.error("File not upated", err)
+        }
+    }
+
+    getLoggingLevel(){
+        return this.loggingLevel
+    }
+    /**
+     * Set the logging level of the logger.
+     * @param {"DEBUG"|"INFO"|"WARN"|"ERROR"} targetLevel 
+     */
+    static setLoggingLevel(targetLevel){
+        if(targetLevel in Logger.LEVELS){
+            Logger.LoggingLevel = Logger.LEVELS[targetLevel]
+        }
     }
 
 
