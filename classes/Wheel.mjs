a1ff06bc27882e81182dcdaa9cab8df8ba655a72
Chris <cgkocur@gmail.com>
2026-06-07 22:48:57 -0400

    adding basic stuff with new logger

--- classes/Wheel.mjs
+++ classes/Wheel.mjs
@@ -1,5 +1,8 @@
+import { Logger } from "./Logger.mjs"
+
 export class Wheel{
-    constructor(sections = []){
+    constructor(logFilePath = "./", sections = []){
+        this.WheelLogger = new Logger(logFilePath, "Wheel")
         this.sections = sections
         this.sectionWidthInDeg = 0
         this.currentDeg = 0
@@ -24,6 +27,7 @@ export class Wheel{
         this.sections = sectionValues
         this.shuffleSections()
         this.sectionWidthInDeg = 360 / this.sections.length
+        this.WheelLogger.info(`Wheel sections generated`)
     }
     /**
      * Generates random values for the board. For right now I don't care much for distribution balance of scores.
@@ -32,21 +36,25 @@ export class Wheel{
      * @returns {number} random number with two zeroes added.
      */
     getRandomValue(min, max){
+        this.WheelLogger.log(`Generated random value`)
         return (Math.floor(Math.random() * (max - min + 1)) + min) * 100
     }
     coinFlip(){
+        this.WheelLogger.log(`Flipping coin`)
         return Math.random() < .5
     }
     shuffleSections(){
-            for (let i = this.sections.length - 1; i > 0; i--) {
+        for (let i = this.sections.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1))
             let temp = this.sections[i]
             this.sections[i] = this.sections[j]
             this.sections[j] = temp
         }
+        this.WheelLogger.info(`Wheel sections have been shuffled.`)
     }
     calcRandomNudge(baseNum){
         let nudgeValue = baseNum * (1 + Math.random())
+        this.WheelLogger.log(`Nudge value of ${nudgeValue}`)
         if (this.coinFlip()){
             return nudgeValue
         } else {
@@ -54,13 +62,14 @@ export class Wheel{
         }
     }
     calcWheelSpinPowerInDegrees(speedFromPhone){
+        this.WheelLogger.log(`Calculating wheel speed from phone data.`)
         // .6 is a stdv of about 8.5 over 10000 spins, which is like +/- one half section
         return Math.round((speedFromPhone * this.speedPowerFactor) + (this.calcRandomNudge(speedFromPhone))) 
     }
     resetCurrentDeg(){
-        console.log(`Current reading: ${this.currentDeg}`)
+        this.WheelLogger.log(`Current reading: ${this.currentDeg}`)
         while (this.currentDeg > 360){
-            console.log(`Lowering from ${this.currentDeg} to ${this.currentDeg - 360}`)
+            this.WheelLogger.log(`Lowering from ${this.currentDeg} to ${this.currentDeg - 360}`)
             this.currentDeg -= 360
         }
         return this.currentDeg
@@ -72,7 +81,7 @@ export class Wheel{
      */
     spinWheel(speedFromPhone){
         let rotation = this.calcWheelSpinPowerInDegrees(speedFromPhone)
-        console.log(`Power Rating: ${rotation}, spinning from ${this.currentDeg} to ${this.currentDeg + rotation}, using speed: ${speedFromPhone}`)
+        this.WheelLogger.log(`Power Rating: ${rotation}, spinning from ${this.currentDeg} to ${this.currentDeg + rotation}, using speed: ${speedFromPhone}`)
         this.currentDeg += rotation
         this.resetCurrentDeg()
         return rotation
